8623c3083567f478920215bdbf95082bbcd103e8
ngiyshhk <ngiys@icloud.com>
2019-05-07 12:54:44 +0900

    feat: add crud methods

--- repository/fuga_repository.go
+++ repository/fuga_repository.go
@@ -4,4 +4,7 @@ import "github.com/ngiyshhk/golang-clean-arch-entity/model"
 
 type FugaRepository interface {
 	Insert(entity *model.Fuga) (bool, error)
+	Update(entity *model.Fuga) (bool, error)
+	Select(ids []int) ([]*model.Fuga, error)
+	Delete(id int) (bool, error)
 }
